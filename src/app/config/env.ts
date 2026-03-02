96e42e2ead9dfb0f837e69f7ff54ca7dbfb1b930
Noornabi <noornabimd9891@gmail.com>
2026-03-02 17:53:12 +0600

    Admin seeding part added

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -12,6 +12,9 @@ interface EnvConfig {
   BETTER_AUTH_URL: string;
   APP_URL: string;
   JWT_SECRET: string;
+  ADMIN_NAME: string;
+  ADMIN_EMAIL: string;
+  ADMIN_PASSWORD: string;
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -23,11 +26,13 @@ const loadEnvVariables = (): EnvConfig => {
     "BETTER_AUTH_URL",
     "APP_URL",
     "JWT_SECRET",
+    "ADMIN_NAME",
+    "ADMIN_EMAIL",
+    "ADMIN_PASSWORD",
   ];
 
   requiredVariable.forEach((variable) => {
     if (!process.env[variable]) {
-
       throw new AppError(
         status.INTERNAL_SERVER_ERROR,
         `Environment vaiable ${variable} is required but not set in .env file!`,
@@ -43,6 +48,9 @@ const loadEnvVariables = (): EnvConfig => {
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
     APP_URL: process.env.APP_URL as string,
     JWT_SECRET: process.env.JWT_SECRET as string,
+    ADMIN_NAME: process.env.ADMIN_NAME as string,
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL as string,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD as string,
   };
 };
 
